f90d7c02b076c06b649ce611ac2d9c7332fcef8c
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-04-18 17:55:54 +0530

    changesadded

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ const db=mysql.createConnection({
     host:"localhost",
     user:'root',
     password:'',
-    database:'bharatpol'
+    database:'bharatpol',
+
 })
 app.get('/',(req,res)=>{
     return res.json("hi hello")
@@ -33,6 +34,32 @@ app.get("/candidates",(req,res)=>{
     return res.json(data)
   })
 })
+app.post("/candidates/add", (req,res)=>{
+  
+  
+ const candidateName= req.body.candidateName
+  const partyName=req.body.partyName
+  const partyLogo=req.body.partyLogo
+  const candidatePhoto=req.body.candidatePhoto
+  const candidateSummary=req.body.candidateSummary
+  const constituencyName=req.body.constituencyName
 
+  const quer = "INSERT INTO candidates (stateName,constituencyName,candidateName,partyName,partyLogo,candidatePhoto,candidateSummary) VALUES (?,?,?,?,?,?,?)"
+  const values={
+    stateName,
+   candidateName,
+   constituencyName,
+    partyName,
+    partyLogo,
+    candidatePhoto,
+    candidateSummary,
+  }
+  console.log(values);
+  db.query(quer,[stateName,candidateName,constituencyName,partyName, partyLogo,candidatePhoto,candidateSummary],(err,data)=>{
+  // db.query(query, [req.body.name, req.body.state,req.body.party,req.body.symbol,req.body.image,req.body.about,req.body.constituency],(err,data)=>{
+    if(err) return res.json(err);
+    return console.log(res.json(data))
+  })
+})
 
 app.listen(8080,()=>{console.log("db running")})
